61814a68f235bb564e4e6a0d51b55e6282246ccf
bettykirii <warwarekirii@gmail.com>
2022-01-20 14:27:30 +0300

    rewrote manifest json and index.jsx to convert it to pwa app

--- frontend/src/components/pix/Pix.jsx
+++ frontend/src/components/pix/Pix.jsx
@@ -1,44 +1,16 @@
 import { useState } from 'react';
 import Head from 'next/head';
 
-import { useFormik } from 'formik';
 import axios from 'axios';
 
 // import Header from '../components/Header';
 // import { useCart } from '../contexts/CartContext';
 
-import { formatPrice } from '../util/format';
 
-export default function Pix() {
-  const [orderStatus, setOrderStatus] = useState('pre-order');
-  const [qrcode, setQrcode] = useState(null);
-  const { cart, removeFromCart, updateProductQuantity } = useCart();
+ function Pix() {
 
-  const form = useFormik({
-    initialValues: {
-      cpf: '',
-      name: '',
-      phone: ''
-    },
-    onSubmit: async (values) => {
-      const items = Object.keys(cart).map((item) => {
-        const formattedItem = {
-          quantity: cart[item].quantity,
-          price: cart[item].data.price,
-          name: cart[item].data.name,
-        };
 
-        return formattedItem;
-      });
-      const order = { ...values, items: [...items] };
-
-      setOrderStatus('ordering');
-
-      const { data } = await axios.post('http://localhost:3001/create-order', order);
-
-      setQrcode(data.qrcode);
-      setOrderStatus('order-received');
-    }
-  });
+  
 
 }
+export default Pix;
