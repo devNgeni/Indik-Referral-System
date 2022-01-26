77fb1cae513ec82afdb1f06557ec8104ef911634
bettykirii <warwarekirii@gmail.com>
2022-01-26 18:43:39 +0300

    rendered the QRcode image to frontend

--- backend/Pix/createCharge.js
+++ backend/Pix/createCharge.js
@@ -3,20 +3,21 @@ const cors = require("cors");
 const path = require("path");
 
 
-const {
-  createPixCharge,
-} = require("./charge/pixCreateImmediateCharge");
+
+const createPixCharge = require("./charge/pixCreateImmediateCharge");
+const { QrCode } = require("./location/pixGenerateQRCode");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-//api that creates the charge
-app.post("/api/v1/createCharge", async (request, response) => {
-  const pixCharge = await createPixCharge(request.body);
-  console.log(response.data)
-  response.send({ ok:1 , pixCharge });
+
+// api that creates the charge
+app.get("/api/v1/createCharge", async (request, response) => {
+  console.log(`body:`, request.body);
+  const pixCharge = await createPixCharge(request.body)
   
+  return response.status(200).json({ success: true,  pixCharge });
 });
 
 
